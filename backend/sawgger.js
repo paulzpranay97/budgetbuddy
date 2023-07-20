6eb349e4145cb7e4d29b3abfd16420d644263ddf
paulzpranay97 <paulpranay1997@gmail.com>
2023-07-21 05:27:36 +0530

    done with users data swagger documentation

--- backend/sawgger.js
+++ backend/sawgger.js
@@ -3,7 +3,7 @@ const YAML = require("yamljs");
 const swaggerJSDocs = YAML.load("api.yaml");
 
 const options = {
-    customCss: ` img {content:url(\'https://drive.google.com/file/d/1sea80WJ387PoL-z_m2FHFxY1nvQJTvT3/view?usp=sharing\'); height:auto;} `,
+    customCss: ` img {content:url(\'../https://drive.google.com/file/d/1sea80WJ387PoL-z_m2FHFxY1nvQJTvT3/view?usp=sharing\'); height:auto;} `,
     customfavIcon: "../favicon.ico",
     customSiteTitle: "Code Improve API Doc"
 }; 
